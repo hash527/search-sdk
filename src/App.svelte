0b69952a333c44adab2c8e12f6a94600593931a5
HashVD <harshad@italentdigital.com>
2023-10-30 13:07:30 +0530

    WIP: Modal UI

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<svelte:options customElement="global-search" />
+
+<script>
+  import searchLogo from "./assets/icons/search.svg";
+  import Modal from "./lib/Modal.svelte";
+  let showModal = false;
+</script>
+
+<div class="searchBox">
+  <div class="searchContainer">
+    <img src={searchLogo} alt="search-logo" height="20px" width="20px" />
+    <button class="btn" on:click={() => (showModal = true)}> Search </button>
+  </div>
+</div>
+
+<Modal bind:showModal/>
---
+++ src/lib/Modal.svelte
@@ -0,0 +1,90 @@
+<script>
+  export let showModal; // boolean
+
+  let dialog; // HTMLDialogElement
+
+  $: if (dialog && showModal) dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+  bind:this={dialog}
+  on:close={() => (showModal = false)}
+  on:click|self={() => dialog.close()}
+>
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div on:click|stopPropagation>
+    <div>
+      <input type="text" placeholder="How to use the platform" />
+    </div>
+    <hr />
+    <ol class="definition-list">
+      <li>of or relating to modality in logic</li>
+      <li>
+        containing provisions as to the mode of procedure or the manner of
+        taking effect —used of a contract or legacy
+      </li>
+      <li>of or relating to a musical mode</li>
+      <li>of or relating to structure as opposed to substance</li>
+      <li>
+        of, relating to, or constituting a grammatical form or category
+        characteristically indicating predication
+      </li>
+      <li>of or relating to a statistical mode</li>
+    </ol>
+
+    <a href="https://www.merriam-webster.com/dictionary/modal"
+      >merriam-webster.com</a
+    >
+
+    <hr />
+    <!-- svelte-ignore a11y-autofocus -->
+    <button autofocus on:click={() => dialog.close()}>close modal</button>
+  </div>
+</dialog>
+
+<style>
+  input {
+    width: 100%;
+    border: none;
+    padding: 10px 0px;
+    outline: none;
+    font-size: large;
+  }
+
+  dialog {
+    max-width: 32em;
+    border-radius: 8px;
+    border: none;
+	justify-content: center;
+  }
+  dialog::backdrop {
+    background: rgba(0, 0, 0, 0.3);
+  }
+
+  dialog[open] {
+    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+  @keyframes zoom {
+    from {
+      transform: scale(0.95);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+  dialog[open]::backdrop {
+    animation: fade 0.2s ease-out;
+  }
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  button {
+    display: block;
+  }
+</style>
